ba4e804704430b05311cc65d982b08f61a5f5a86
Kersten Schaller <kersten.schaller@outlook.com>
2026-02-14 18:13:23 +0100

    update

--- test3.lua
+++ test3.lua
@@ -10,6 +10,7 @@ local script_dir = dfhack.getDFPath() .. '/dfhack-config/scripts/'
 package.path = script_dir .. '?.lua;' .. script_dir .. '?/init.lua;' .. package.path
 
 local LogParser = require('LogParser')
+local DiagramScreen = require('DiagramWindow')
 
 local view
 
@@ -27,23 +28,22 @@ end
 
 function StatsTextWidget:init()
 	self:addviews{
-		widgets.Label{
-			view_id='statsPageLabel',
-			frame={t=0,l=0},
-			text="Stats Text Widget "..tostring(currentIndex),
-		},
-	}
+
+
+			}
 end
 
-function StatsTextWidget:onRenderBody(dc)
-	self.subviews.statsPageLabel:setText("Stats Text Widget "..tostring(currentIndex))
-	self:updateLayout()
-end
+local WindowWidth = 85
+local WindowHeight = 40
+local WindowPosLeft = 40
+local WindowPosTop = 13
+
+
 
 StatsWindow = defclass(StatsWindow, widgets.Window)
 StatsWindow.ATTRS {
-	frame_title = 'Test Window 2',
-	frame={w=80, h=40, l=40, t=13},
+	frame_title = 'Fortress Statistics',
+	frame={w=WindowWidth, h=WindowHeight, l=WindowPosLeft, t=WindowPosTop},
 	autoarrange_subviews = true,
 	autoarrange_gap = 1,
 	resizable = true,
@@ -52,32 +52,68 @@ StatsWindow.ATTRS {
 
 
 LogParser.parseAll()
-local years = LogParser.getYears()
+local years = {"All", table.unpack(LogParser.getYears())}
 --local years = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"}
 
 
 function StatsWindow:init()
+
+	local longInitText = ""
+	-- fill longInitText with a very long string to test scrolling	
+	for i = 1, 100 do
+		longInitText = longInitText .. "This is line " .. i ..'-------------------------------------------------------' .."\n"
+	end
+
 	self:addviews{
-		widgets.TabBar{
-            labels=years,
-            on_select=function(idx)
-					currentIndex = idx
-					StatsTextWidget:setCurrentIndex(idx)
-					self:updateLayout()
-				end,
-			get_cur_page=function() return StatsTextWidget:getCurrentIndex() end,
-        },
-		widgets.Divider{
-			frame_style_l=false,
-            frame_style_r=false,
-            frame={t=3,l=0,r=0,h=1},
-		},	
-		StatsTextWidget{
-			view_id='statsTextWidget',
-			frame={t=4,l=0,r=0,b=0},
+
+		widgets.Panel{
+			view_id='yearsPanel',
+			frame={t=0,l=0,w=6},
+			frame_style=gui.FRAME_INTERIOR,
+			subviews={
+				widgets.Label{
+					frame={t=0,l=0},
+					text="Years",
+				},
+				widgets.Divider{
+					frame_style_l=false,
+					frame_style_r=false,
+					frame={t=1},
+				},
+				widgets.List{
+				view_id='yearList',
+				frame={t=3,l=0},
+				choices=years,
+				on_submit=function(idx, choice)
+					self.subviews.statsTextWidget:setCurrentIndex(idx)
+				end
+				},
+			},
+			
+			
 		},
+		widgets.TabBar{
+			view_id='tabBar',
+			frame={t=0,l=8},
+			labels={'Overview', 'Population', 'Economy','Labor', 'Artifacts','Warfare'},
+			on_select=function(idx) currentIndex = idx end,
+			get_cur_page=function() return currentIndex end
+		},		
+		widgets.Panel{
+			frame_style=gui.FRAME_INTERIOR,
+			frame={t=3,l=7,b=0},
+			subviews={
+				widgets.WrappedLabel{
+					view_id='statsTextWidget',
+					frame={t=0,l=0,w=WindowWidth-7,h=WindowHeight-5},
+					text_to_wrap=longInitText,
+				}
+			}
+			}
 	}
-end
+	end
+
+
 
 function StatsWindow:onRenderBody(dc)
 	--self.subviews.statsTextWidget:setCurrentIndex(currentIndex)
